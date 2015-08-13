14888aa0392c956893a211271234e5eed012f0ea
andylockran <andy@zrmt.com>
2015-08-13 22:32:19 +0100

    turned on postgres

--- cycleangel/cycleangel/settings/base.py
+++ cycleangel/cycleangel/settings/base.py
@@ -100,10 +100,20 @@ WSGI_APPLICATION = 'cycleangel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cycleangel',
     }
 }
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+    
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 
 # Internationalization

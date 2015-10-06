13d1f0ca2334b2ffbba98becd6a68e269b1a6ce1
andylockran <andy@zrmt.com>
2015-10-06 03:36:28 +0100

    Updated homepage

--- cycleangel/home/models.py
+++ cycleangel/home/models.py
@@ -4,19 +4,21 @@ from django.db import models
 
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField 
-from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel
+from wagtail.wagtailcore.fields import StreamField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel, StreamFieldPanel
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtailimages.blocks import ImageChooserBlock
 
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
-
-    column1 = RichTextField(blank=True)
-    column2 = RichTextField(blank=True)
-    column3 = RichTextField(blank=True)
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ])
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
+        StreamFieldPanel('body')
         ]
     promote_panels = [
         MultiFieldPanel(Page.promote_panels, "Common page configuration"),
